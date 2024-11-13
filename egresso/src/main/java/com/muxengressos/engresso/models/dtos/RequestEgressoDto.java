3ff20cbb546f97d5bb9d609b8686bd4fbdd2910a
Railton-Carvalho <railtonplayrx@gmail.com>
2024-11-13 11:46:52 -0300

    refactor: Egresso main DTO

--- egresso/src/main/java/com/muxengressos/engresso/models/dtos/RequestEgressoDto.java
+++ egresso/src/main/java/com/muxengressos/engresso/models/dtos/RequestEgressoDto.java
@@ -9,18 +9,42 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@JsonView
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RequestEgressoDto implements Serializable {
 
+    public interface EgressoView{
+        public static interface RegistrationPost{}
+        public static interface EgressoUpdate {}
+        public static interface PasswordUpdate {}
+        public static interface ImageUpdate {}
+    }
+
+    private Integer id;
+
     @NotBlank
+    @JsonView({EgressoView.RegistrationPost.class, EgressoView.EgressoUpdate.class})
     private String nome;
 
     @NotBlank
+    @JsonView({EgressoView.RegistrationPost.class, EgressoView.EgressoUpdate.class})
+    private String cpf;
+
+    @NotBlank
+    @JsonView({EgressoView.RegistrationPost.class, EgressoView.EgressoUpdate.class})
     private String email;
 
     @NotBlank
+    @JsonView({EgressoView.RegistrationPost.class, EgressoView.PasswordUpdate.class})
+    private String senha;
+
+    @NotBlank
+    @JsonView(EgressoView.PasswordUpdate.class)
+    private String old_senha;
+
     private String resumo;
+
+    @JsonView({EgressoView.ImageUpdate.class})
+    private String url_foto;
 
 
 }
